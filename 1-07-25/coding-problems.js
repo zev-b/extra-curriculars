6398403d788cff2a5bf4a18134119adcbb5c72ea
zev-b <zevbistritz@gmail.com>
2025-01-07 18:06:28 -0500

    coding time

---
+++ 1-07-25/coding-problems.js
@@ -0,0 +1,14 @@
+/*
+Problem Statement:
+
+* Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to the target.
+
+?    Input: nums = [2, 7, 11, 15], target = 9
+?    Output: [0, 1]
+?    Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
+Constraints:
+
+^- Each input will have exactly one solution.
+^- You cannot use the same element twice.
+*/
