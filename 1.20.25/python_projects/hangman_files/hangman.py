825a312e3529a801b44e97374dbfd1eebcff77d6
zev-b <zevbistritz@gmail.com>
2025-01-21 13:05:22 -0500

    finished hangman project

--- 1.20.25/python_projects/hangman_files/hangman.py
+++ 1.20.25/python_projects/hangman_files/hangman.py
@@ -15,8 +15,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    while len(word_letters) > 0:
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
         print('You have used these letters: ', ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word:', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
@@ -24,14 +29,22 @@ def hangman():
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
+            else:
+                lives = lives - 1
+                print('Letter is not in word.')
+
         elif user_letter in used_letters:
             print('Nice guess, but you have tried this letter already!')
 
         else:
             print('That won\'t bring him back...\n Invalid Chararcter Submission')
 
+    if lives == 0:
+        print('You died, The word was', word)
+    else:
+        print('You guesses the word correctly', word, '!!')
 
+# user_input = input('Type something:')
+# print(user_input)
 
-
-user_input = input('Type something:')
-print(user_input)
+hangman()
