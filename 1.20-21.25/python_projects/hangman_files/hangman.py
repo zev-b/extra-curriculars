2c07e360adefec7484ecca96e311c288e955ac52
zev-b <zevbistritz@gmail.com>
2025-01-21 13:54:39 -0500

    Tic Tac Toe project

---
+++ 1.20-21.25/python_projects/hangman_files/hangman.py
@@ -0,0 +1,50 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word:', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print('Letter is not in word.\n')
+
+        elif user_letter in used_letters:
+            print('Nice guess, but you have tried this letter already!\n')
+
+        else:
+            print('That won\'t bring him back...\n Invalid Chararcter Submission')
+
+    if lives == 0:
+        print('You died, The word was', word)
+    else:
+        print('You guesses the word correctly', word, '!!')
+
+# user_input = input('Type something:')
+# print(user_input)
+
+hangman()
