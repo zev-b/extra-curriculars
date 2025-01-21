709524ec69376c57b7a30d66898d0b5efb3e63df
zev-b <zevbistritz@gmail.com>
2025-01-21 13:55:24 -0500

    Tic Tac Toe project

---
+++ 1.20-21.25/python_projects/tic_tac_toe/game.py
@@ -0,0 +1,22 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.cuurent_winner = None
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+    
+
+def play():
+    pass
+
