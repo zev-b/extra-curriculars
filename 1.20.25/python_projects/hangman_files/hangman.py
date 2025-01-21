bfdda7c33fb623ede914b966d266be672237e7d2
zev-b <zevbistritz@gmail.com>
2025-01-20 19:43:38 -0500

    mid-hangman project

---
+++ 1.20.25/python_projects/hangman_files/hangman.py
@@ -0,0 +1,37 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    while len(word_letters) > 0:
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('Nice guess, but you have tried this letter already!')
+
+        else:
+            print('That won\'t bring him back...\n Invalid Chararcter Submission')
+
+
+
+
+user_input = input('Type something:')
+print(user_input)
