acb31c907238b024e4070fde5f15f1bec1e096db
zev-b <zevbistritz@gmail.com>
2025-05-26 12:08:08 -0400

    adds

---
+++ NeetCode/5.26.25.js
@@ -0,0 +1,38 @@
+/*
+    Example: input = [5, 3, 6, 2, 4, null, null, 1]
+
+                     5
+                    / \
+                   3   6
+                  / \
+                 2   4
+                /
+               1
+*/
+
+const kthSmallest = (root, k) => {
+    const inOrder = [];
+    dfs(root, inOrder);
+    return inOrder[k - 1];
+}
+
+const dfs = (node, k, inOrder) => {
+    if (!node) return;
+    dfs(node.left, k, inOrder);
+    inOrder.push(node.val);
+    dfs(node.right, inOrder);
+}
+
+
+/**
+ * Validate Binary Search Tree
+ *  [ ALL left subtree(s) nodes are less than, All right subtree(s) are greater than. ]
+ * 
+ *          5
+ *         / \
+ *        3   7
+ *           / \
+ *          4   8
+ * 
+ *  ans: False
+ */
